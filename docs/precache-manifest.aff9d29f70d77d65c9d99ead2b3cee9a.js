c4fd99bb6ad25b35f88c0ade3ecd89acd53e8800
IITA WEB <iita.comms@gmail.com>
2022-04-30 15:48:52 +0100

    testimonyismine
    initial

---
+++ docs/precache-manifest.aff9d29f70d77d65c9d99ead2b3cee9a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f630e53b4de083f15f75024be619dd7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c21ca6648b55abf8bb0",
+    "url": "/static/css/2.bb8e5bd3.chunk.css"
+  },
+  {
+    "revision": "048965c59ad781838a84",
+    "url": "/static/css/main.283e5761.chunk.css"
+  },
+  {
+    "revision": "8c21ca6648b55abf8bb0",
+    "url": "/static/js/2.b3bf5a82.chunk.js"
+  },
+  {
+    "revision": "048965c59ad781838a84",
+    "url": "/static/js/main.e95be79a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
