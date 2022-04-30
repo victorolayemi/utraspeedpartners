61343d72202eaad633a3728795e817a80ecc145f
IITA WEB <testimonyismine@gmail.com>
2022-04-30 17:26:27 +0100

    m

---
+++ docs/precache-manifest.95379b88cd78e5e6e33df904f7c8effa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759f1f3e048d756fbec473979c6c7ea6",
+    "url": "/utraSpeedpartners/index.html"
+  },
+  {
+    "revision": "557e6bfcf5b367289444",
+    "url": "/utraSpeedpartners/static/css/2.d1d3a1c7.chunk.css"
+  },
+  {
+    "revision": "8c85b8938a5b990dce7f",
+    "url": "/utraSpeedpartners/static/css/main.283e5761.chunk.css"
+  },
+  {
+    "revision": "557e6bfcf5b367289444",
+    "url": "/utraSpeedpartners/static/js/2.78f270ea.chunk.js"
+  },
+  {
+    "revision": "8c85b8938a5b990dce7f",
+    "url": "/utraSpeedpartners/static/js/main.8782c1f0.chunk.js"
+  },
+  {
+    "revision": "91962c10dbc7fa5df234",
+    "url": "/utraSpeedpartners/static/js/runtime~main.7deda145.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/utraSpeedpartners/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/utraSpeedpartners/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/utraSpeedpartners/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/utraSpeedpartners/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.788cbac6402fdf31d25dc846ce368095.js"
+  "/utraSpeedpartners/precache-manifest.95379b88cd78e5e6e33df904f7c8effa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/utraSpeedpartners/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
